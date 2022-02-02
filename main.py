6d65e75c87ff172b137d79da2a608ac5084da33d
Shoubhit Dash <shoubhit2005@gmail.com>
2022-02-02 20:43:23 +0530

    now using rich lib to make it look nice in terminal

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 import spotipy
 import os
 import dotenv
-from methods import *
 import speech_recognition as sr
 from spotipy.oauth2 import SpotifyOAuth
+from rich import print
+from methods import *
 
 
 # load environment variables
@@ -31,7 +32,7 @@ while True:
     # set-up speech recognizer
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Ready!")
+        print("[bold deep_sky_blue2]Ready![/bold deep_sky_blue2]")
         audio = r.listen(source)
 
     command = None
@@ -39,7 +40,7 @@ while True:
     try:
         command = r.recognize_google(audio_data=audio).lower()
     except sr.UnknownValueError:
-        print("Could not understand.")
+        print("[italic red]Could not understand.[/italic red]")
         continue
     
     print(command)
@@ -48,7 +49,7 @@ while True:
     
     # checking if the speech recognizer recognized a command
     if len(words) <= 1:
-        print("Could not understand.")
+        print("[italic red]Could not understand.[/italic red]")
         continue
     
     # here action is the command, eg: 'play', 'album' or 'artist'
@@ -61,25 +62,25 @@ while True:
         if action == "play":
             uri = get_track_uri(spotify=sp, name=name)
             play_track(spotify=sp, uri=uri)
-            print(f"Playing {name}...")
+            print(f"[bold deep_sky_blue2]Playing track:[/bold deep_sky_blue2] [italic spring_green3]{name}...[/italic spring_green3]")
 
         elif action == "album":
             uri = get_album_uri(spotify=sp, name=name)
             play_album(spotify=sp, uri=uri)
-            print(f"Playing album {name}...")
+            print(f"[bold deep_sky_blue2]Playing album:[/bold deep_sky_blue2] [italic spring_green3]{name}...[/italic spring_green3]")
         
         elif action == "artist":
             uri = get_artist_uri(spotify=sp, name=name)
             play_artist(spotify=sp, uri=uri)
-            print(f"Playing artist {name}...")
+            print(f"[bold deep_sky_blue2]Playing artist:[/bold deep_sky_blue2] [italic spring_green3]{name}...[/italic spring_green3]")
         
         elif action == "skip":
             skip_track(spotify=sp)
-            print("Skipped current track...")
+            print("[italic orange1]Skipped current track...[/italic orange1]")
         
         elif action == "previous":
             previous_track(spotify=sp)
-            print("Playing previous track...")
+            print("[italic orange1]Playing previous track...[/italic orange1]")
 
     except InvalidSearchError:
-        print(f"Could not find {name}. Try again.")
+        print(f"[italic red]Could not find {name}. Try again.[/italic red]")
